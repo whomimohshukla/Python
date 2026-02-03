fd8efc10c239d9bc3f2894474481330a5194963d
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-04 04:57:15 +0530

    add loops examples demonstrating iteration and conditional checks

---
+++ basics/loops.py
@@ -0,0 +1,23 @@
+
+
+# for variable in sequence:
+#     code
+
+
+for i in range(5):
+    print(i)
+
+
+
+
+# range () function 
+# range(5)
+# range(1,5)
+# range(1,10,2)
+
+
+for i in range(1,11):
+    if i%2==0:
+        print(i,"is even")
+    else:
+        print(i,"is odd")
