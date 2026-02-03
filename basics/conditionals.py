8b95719f8a25d816702a3603f2e2083759f53d39
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-04 04:53:30 +0530

    add conditionals examples and tasks for user input and decision making

---
+++ basics/conditionals.py
@@ -0,0 +1,33 @@
+# What are Conditions?
+
+# Conditions help the program make decisions.
+
+# Real life:
+
+# If it rains → take umbrella
+# Else → go normally
+
+
+
+# if condition:
+#     code
+
+
+age=20
+
+if age>=18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
+
+
+marks=75
+
+if marks>=60:
+    print("You passed")
+elif marks>=50:
+    print("You passed with some difficulty")
+else:
+    print("You failed")
+    
+    
---
+++ basics/conditonals.task.py
@@ -0,0 +1,24 @@
+
+
+# task-1
+
+
+num=int(input("Enter a number: "))
+
+if num>0:
+    print("The number is positive")
+elif num<0:
+    print("The number is negative")
+else:
+    print("The number is zero")
+
+
+#task-2
+
+name=input("Enter your name: ")
+age=int(input("Enter your age: "))
+
+if age>=18:
+    print("You are eligible to vote")
+else:
+    print("You are not eligible to vote")
