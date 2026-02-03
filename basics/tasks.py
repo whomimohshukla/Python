cc550c1dafc672461d3b7880b388e82e273d3409
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-04 04:44:16 +0530

    add tasks and variables scripts for user input and basic operations

---
+++ basics/tasks.py
@@ -0,0 +1,12 @@
+name=input("Enter your name:")
+age=int(input("Enter your age:"))
+wight=float(input("Enter your wight:"))
+
+print("My name is ",name,"and I am",age,"years old and my wight is",wight,"kg")
+
+num1=int(input("Enter your first number:"))
+num2=int(input("Enter your second number:"))
+
+sum=num1+num2
+
+print("The sum of",num1,"and",num2,"is",sum)
---
+++ basics/variables.py
@@ -0,0 +1,26 @@
+# name ="Mimohshukla"
+
+# age=21
+
+# is_student = True
+
+# print(name)
+# print(age)  
+# print(is_student)
+
+
+# x = 10
+# # print(type(x))
+
+# print("Python is messaged based on the type of the data")
+
+
+# name=input("Enter your name:")
+
+# print("Hello",name)
+
+
+# age=int(input("Enter your age:"))
+
+# print(type(age))
+
