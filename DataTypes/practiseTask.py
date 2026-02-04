4232a52cdcaaea00977ca334233ebdf1ec5ce06c
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-05 03:44:07 +0530

    add initial example demonstrating basic operations with lists and tuples

---
+++ DataTypes/practiseTask.py
@@ -0,0 +1,15 @@
+#Task-1
+
+numbers=[10,20,30,40,50]
+
+print("Sum",sum(numbers))
+
+print("Max",max(numbers))
+
+
+cities = ("Delhi", "Mumbai", "Bangalore")
+
+for city in cities:
+    print(city)
+
+
