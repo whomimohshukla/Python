96398d4957a169fe76712fdf666e8fcd57d83465
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-05 03:43:58 +0530

    add example demonstrating looping through lists and dictionaries

---
+++ DataTypes/loopingDatatypes.py
@@ -0,0 +1,21 @@
+# Looping through datatypes
+
+
+list = [10, 20, 30, 40, 50]
+
+for i in list:
+    print(i)
+
+
+
+# looping through a dictionary
+
+student = {
+    "name": "Alice",
+    "age": 21,
+    "courses": ["Math", "Science", "Art"],
+}
+
+for key in student:
+    print(key)
+    print(student[key])
