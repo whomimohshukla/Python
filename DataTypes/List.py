b39e0fea11f7709b1d8595adf8ccf056c6df0dac
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-04 23:06:38 +0530

    add examples and explanations for lists, including mutability and indexing

---
+++ DataTypes/List.py
@@ -0,0 +1,30 @@
+# What is a List?
+
+# Ordered
+
+# Changeable (mutable)
+
+# Allows duplicates
+
+
+numbers=[10,20,30,40,50]
+
+names=["Mimoh","Shukla","Mimoh"]
+
+mixed=[10,"Mimoh",True,3.14]
+
+print(numbers)
+
+print(names)
+
+print(mixed)
+
+print(numbers[0])   # 10
+print(numbers[-1])  # 40
+
+
+
+numbers[1] = 25
+print(numbers)
+
+
