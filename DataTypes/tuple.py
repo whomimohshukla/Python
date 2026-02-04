0e0d444ec30c4a8fa8997cd74129443c2b4d58bc
Mimohshukla00 <mimohshukla0001@gmail.com>
2026-02-05 03:44:24 +0530

    add example demonstrating tuple properties, including immutability and ordering

---
+++ DataTypes/tuple.py
@@ -0,0 +1,15 @@
+# TUPLE
+# 🔹 What is a Tuple?
+
+# Ordered
+
+# Unchangeable (immutable)
+
+# Faster than list
+
+
+point =(10,20)
+print(point)
+
+point[0] = 30  # This will raise an error
+print(point[0])   # 10
